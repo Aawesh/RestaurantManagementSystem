6717967964bedc246a325d0c80e3b6666e02b5a5
Aawesh Shrestha <shrestha.aawesh@gmail.com>
2017-11-12 20:38:07 -0600

    added CRUD for food

---
+++ RestaurantManagementSystem/Controllers/FoodController.cs
@@ -0,0 +1,114 @@
+﻿using RestaurantManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantManagementSystem.Controllers
+{
+    public class FoodController : Controller
+    {
+        FoodEntities foodEntities = new FoodEntities();
+        // GET: Food
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult InsertFood()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult InsertFood(Food food)
+        {
+            string Message = "";
+
+            if (ModelState.IsValid)
+            {
+                foodEntities.Foods.Add(food);
+                foodEntities.SaveChanges();
+
+                Message = "Successful food entry";
+                ViewBag.Message = Message;
+
+                return RedirectToAction("InsertFood");
+               
+            }
+            return View(food);
+        }
+
+        [Authorize]
+        public ActionResult DisplayFoodList()
+        {
+            return View(foodEntities.Foods.ToList());
+        }
+
+        [Authorize]
+        public ActionResult DeleteFood(int id=0)
+        {
+            Food food = foodEntities.Foods.Find(id);
+            if (food == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(food);
+        }
+
+        [HttpPost, ActionName("DeleteFood")]
+        [Authorize]
+        public ActionResult DeleteConfirmed(int id=0)
+        {
+            Food food = foodEntities.Foods.Find(id);
+            foodEntities.Foods.Remove(food);
+            foodEntities.SaveChanges();
+            return RedirectToAction("DisplayFoodList");
+        }
+
+        [Authorize]
+        public ActionResult FoodDetails(int id = 0)
+        {
+            Food food = foodEntities.Foods.Find(id);
+            if (food == null)
+            {
+                return HttpNotFound();
+            }
+            return View(food);
+        }
+
+        [Authorize]
+        public ActionResult EditFood(int id = 0)
+        {
+            Food food = foodEntities.Foods.Find(id);
+            if (food == null)
+            {
+                return HttpNotFound();
+            }
+            return View(food);
+        }
+        
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditFood(Food food)
+        {
+            if (ModelState.IsValid)
+            {
+                foodEntities.Entry(food).State = EntityState.Modified;
+                foodEntities.SaveChanges();
+                return RedirectToAction("DisplayFoodList");
+            }
+            return View(food);
+        }
+
+
+    }
+}
